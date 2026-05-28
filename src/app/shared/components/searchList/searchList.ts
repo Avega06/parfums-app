7b29c0f39141b192c11a52371361d4f2c53510a1
Alex Vega <alexvega06.1996@gmail.com>
2026-05-27 20:30:33 -0400

    Close drawer for mobiles fixed

--- src/app/shared/components/searchList/searchList.ts
+++ src/app/shared/components/searchList/searchList.ts
@@ -4,9 +4,11 @@ import {
   computed,
   inject,
   input,
+  signal,
+  afterNextRender,
 } from '@angular/core';
 import { type ProductItem } from '../../../intefaces';
-import { Router, RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'search-list',
@@ -15,8 +17,10 @@ import { Router, RouterLink } from '@angular/router';
 })
 export class SearchList {
   products = input.required<ProductItem[]>();
-
   router = inject(Router);
+
+  // Señal para controlar si debemos aplicar el comportamiento del drawer
+  isMobile = signal(false);
 
   encodedProducts = computed(() =>
     this.products().map((product) => ({
@@ -24,6 +28,18 @@ export class SearchList {
       encodedName: encodeURIComponent(product.name),
     })),
   );
+
+  constructor() {
+    afterNextRender(() => {
+      const mediaQuery = window.matchMedia('(max-width: 1023px)');
+
+      this.isMobile.set(mediaQuery.matches);
+
+      mediaQuery.addEventListener('change', (e) => {
+        this.isMobile.set(e.matches);
+      });
+    });
+  }
 
   goToProduct(name: string) {
     console.log('Navegando a:', name);
