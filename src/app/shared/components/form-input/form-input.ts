3b84c401f112ef2413d403b406ae733395cec710
Alex Vega <alexvega06.1996@gmail.com>
2026-06-15 21:37:12 -0400

    Add - Password hint info

--- src/app/shared/components/form-input/form-input.ts
+++ src/app/shared/components/form-input/form-input.ts
@@ -1,4 +1,4 @@
-import { Component, effect, input, signal } from '@angular/core';
+import { Component, computed, effect, input, signal } from '@angular/core';
 import { Field, FormField, ValidationError } from '@angular/forms/signals';
 
 @Component({
@@ -13,18 +13,23 @@ export class FormInput {
   formField = input.required<Field<any, string | number>>();
   type = input.required<string>();
   formClass = input.required<string>();
+
   isTouched = input<boolean>();
+  isPasswordInfo = input<boolean>();
 
   placeholder = input<string>();
   errors = input<ValidationError.WithFieldTree[]>();
-
-  ce = effect(() => {
-    console.log(this.errors());
-  });
 
   showPassword = signal(false);
 
   toggleVisibility() {
     this.showPassword.update((value) => !value);
   }
+
+  passwordInfo = computed(() => [
+    'Debe tener más de 8 caracteres, incluyendo',
+    '- Al menos un número',
+    '- Al menos una letra minúscula',
+    '- Al menos una letra mayúscula`',
+  ]);
 }
